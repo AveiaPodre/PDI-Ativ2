83e682580897a7bba343581d82ff4414e04a4521
AveiaPodre <av_jr@outlook.com.br>
2024-01-18 19:38:57 -0300

    unsharp e highboost resolvidos

---
+++ highboost.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter
+
+def highboost(image, enhance_factor=2):
+    image = image.convert('L')
+
+    # Aplicação do filtro passa-baixa (suavização)
+    low_pass = image.filter(ImageFilter.GaussianBlur(5))
+
+    # Converte as imagens para arrays NumPy
+    image_array = np.array(image, dtype=float)
+    low_pass_array = np.array(low_pass, dtype=float)
+
+    # Cálculo da imagem passa-alta (componentes de alta frequência)
+    high_pass = image_array - low_pass_array
+
+    # Aplicação do fator de realce
+    enhanced_image = np.array(image) + enhance_factor * high_pass
+
+    # Ajuste do limite dos valores para o intervalo entre [0, 255] e conversão novamente para imagem
+    enhanced_image = np.clip(enhanced_image, 0, 255).astype(np.uint8)
+    enhanced_image = Image.fromarray(enhanced_image)
+
+    return enhanced_image
+
+lena = Image.open('lena_gray.bmp')
+lena_highboost = highboost(lena)
+lena_highboost.save('lena_highboost.bmp')
+
+plt.imshow(np.array(lena_highboost), cmap='gray')
+plt.show()
--- unsharp.py
+++ unsharp.py
@@ -2,19 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
 
-def unsharp_masking(image, k=1.5, radius=1):
+def unsharp_masking(image, k=1, radius=1):
     image = image.convert("L")
-    
-    #aplicação do filtro gaussiano na imagem
+
+    # Aplicação do filtro gaussiano na imagem
     blurred_image = image.filter(ImageFilter.GaussianBlur(radius=radius))
     
-    #subtração da imagem borrada pela original
-    difference = np.array(image) - np.array(blurred_image)
-     
-    #multiplicação da diferença obtida pelo fator e então soma-se à imagem original
-    sharpened_array = np.array(image) + k * difference
+    # Converte as imagens para arrays NumPy
+    image_array = np.array(image, dtype=float)
+    blurred_image_array = np.array(blurred_image, dtype=float)
 
-    #ajuste do limite dos valores para o intervalo entre [0,255] e converte novamente para imagem
+    # Subtração da imagem borrada pela original
+    difference = image_array - blurred_image_array
+
+    # Multiplicação da diferença obtida pelo fator e então soma-se à imagem original
+    sharpened_array = image_array + k * difference
+
+    # Ajuste do limite dos valores para o intervalo entre [0, 255] e conversão novamente para imagem
     sharpened_array = np.clip(sharpened_array, 0, 255) 
     sharpened_image = Image.fromarray(sharpened_array.astype(np.uint8))
     
